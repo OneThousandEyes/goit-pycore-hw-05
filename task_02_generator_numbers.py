da368a897730aea70d0212f7181b5687c1c46f9f
Vitalii Cherpak <cherpak1985@gmail.com>
2025-10-26 17:18:53 +0200

    task_02 - add solution

---
+++ task_02_generator_numbers.py
@@ -0,0 +1,26 @@
+import re
+from typing import Callable
+
+
+def generator_numbers(text: str):
+    '''Generator function that extracts floating-point numbers from a given text.'''
+    pattern = r'(?<=\s)-?\d+(?:.\d+)?(?=\s)'
+    numbers = re.findall(pattern, text)
+    for number in numbers:
+        yield float(number)
+
+
+def sum_profit(text: str, func: Callable):
+    '''Function that sums up all floating-point numbers extracted by the generator function.'''
+    total = 0
+    for number in func(text):
+        total += number
+    return total
+
+
+text = '''Загальний дохід працівника складається з декількох частин: 1000.01 як
+основний дохід, доповнений додатковими надходженнями 232 і 324.00000
+доларів та витратами на оренду офісного приміщення -222.22 доларів.'''
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
